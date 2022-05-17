ead210368c5fed9d0f3f0309b359096badc52e62
Samantha Acuña <samantha23@estudiantec.cr>
2022-05-17 16:34:59 -0600

    Actualizacion de Net

---
+++ Estrategias_De_Diseño/src/estrategias_de_diseño/Estrategias_De_Diseño.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estrategias_de_diseño;
+
+/**
+ *
+ * @author Sam
+ */
+public class Estrategias_De_Diseño {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
